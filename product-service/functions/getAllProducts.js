da266018cbeb7afbc4ad31ea2eb13d5a6a711f51
Yura Lapitski <lyurik@tut.by>
2022-06-11 13:16:32 +0300

    feat: add functions product-service

---
+++ product-service/functions/getAllProducts.js
@@ -0,0 +1,15 @@
+import products from './productList.json';
+
+const handlerAllProducts = (products = {}, status = 200) => { 
+  return {
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Methods': '*',
+      'Access-Control-Allow-Origin': '*',
+    },
+    statusCode: status,
+    body: JSON.stringify(products),
+  }
+};
+
+export const handler = async (_event) => await handlerAllProducts(products);
---
+++ product-service/functions/getProductByID.js
@@ -0,0 +1,24 @@
+import products from './productList.json';
+
+const handlerGetByID = (products = {}, status = 200) => { 
+  return {
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Methods': '*',
+      'Access-Control-Allow-Origin': '*',
+    },
+    statusCode: status,
+    body: JSON.stringify(products),
+  }
+};
+
+export const handler = async (event) => {
+  const { productId } = event.pathParameters || null;
+
+  const product = await products.find(({ id }) => id === productId);
+  
+  if (!product)  { 
+    return handlerGetByID({ message: 'Error: Product not found!' }, 400);
+  }
+    return handlerGetByID({ ...product }, 200);
+};
