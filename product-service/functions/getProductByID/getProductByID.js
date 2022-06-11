11d10e30073ea3e92c89905f4d5c8161ca1a71d4
Yura Lapitski <lyurik@tut.by>
2022-06-11 21:35:48 +0300

    feat: edit serverless.yaml and functions

--- product-service/functions/getProductByID/getProductByID.js
+++ product-service/functions/getProductByID/getProductByID.js
@@ -1,11 +1,9 @@
-import products from './productList.json';
+import products from '../productList.json';
 
 const handlerGetByID = (products = {}, status = 200) => ( 
   {
     headers: {
       'Content-Type': 'application/json',
-      'Access-Control-Allow-Methods': '*',
-      'Access-Control-Allow-Origin': '*',
     },
     statusCode: status,
     body: JSON.stringify(products),
