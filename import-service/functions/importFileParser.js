a78aaa0dcbf04412db4f485fe3f97b40a9db724d
Yura Lapitski <lyurik@tut.by>
2022-07-03 23:33:57 +0300

    feat: add functions, modules npm, edit serverless.yaml

---
+++ import-service/functions/importFileParser.js
@@ -0,0 +1,7 @@
+import * as sdkS3 from 'aws-sdk/clients/s3';
+import parse from 'csv-parser';
+
+export const handler = async event => {
+
+  
+}
---
+++ import-service/functions/importProductsFile.js
@@ -0,0 +1,4 @@
+import * as sdkS3 from 'aws-sdk/clients/s3';
+
+export const handler = async event => {
+}
