acde44e32b5717dedb87d774f5ddd2e7a39ce397
Yura Lapitski <lyurik@tut.by>
2022-07-05 13:45:59 +0300

    feat: add importProductsFile.js

--- import-service/functions/importProductsFile.js
+++ import-service/functions/importProductsFile.js
@@ -1,4 +1,21 @@
-import * as sdkS3 from 'aws-sdk/clients/s3';
+import AWS from "aws-sdk";
+const BUCKET = "yura703-task5";
 
-export const handler = async event => {
-}
+export const importProductsFile = async event => {
+  const s3 = new AWS.S3({ region: "eu-west-1" });
+  const { name } = event.queryStringParameters;
+  const params = {
+    Bucket: BUCKET,
+    Key: `uploaded/${name}`,    
+    Expires: 120,
+    ContentType: "text/csv"
+  };
+
+  const body = JSON.stringify(s3.getSignedUrl("putObject", params));
+
+  return {
+    statusCode: 201,
+    headers: { "Access-Control-Allow-Origin": "*" },
+    body: body,
+  };
+};
